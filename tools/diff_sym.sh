246ddec745d87ce96ec5b988c005f6ff6790387b
Grien25 <96752609+Grien25@users.noreply.github.com>
2025-07-14 00:40:22 -0700

    Removal

--- tools/diff_sym.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-git --no-pager diff --no-index \
-	<(build/binutils/powerpc-eabi-objdump -D --disassemble-symbols=$1 build/SZBE69/main.elf) \
-	<(build/binutils/powerpc-eabi-objdump -D --disassemble-symbols=$1 ignore/orig.elf)
